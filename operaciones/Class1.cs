666db0ef6259e05d6bca2972c84161ba1c905676
Joseph Murillo <murillojoseph917@gmail.com>
2024-05-02 11:52:34 -0500

    subiendooo

---
+++ operaciones/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace operaciones
+{
+    public class OperacionesBasicas
+    {
+        // función para sumar
+        public int Suma(int a, int b)
+        {
+            return a + b; // retorna el resultado de la suma
+        }
+
+        // función para multiplicar
+        public int Multiplicacion(int a, int b)
+        {
+            return a * b; // retorna el resultado de la multiplicación
+        }
+    }
+}
